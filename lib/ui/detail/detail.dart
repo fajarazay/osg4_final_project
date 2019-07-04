9368bdf2cdd89f95251957822778d4df3d39efd5
Fajar Septian <fajarajay10@gmail.com>
2019-07-04 20:57:29 +0700

    finishing detail screen :tada:

--- lib/ui/detail/detail.dart
+++ lib/ui/detail/detail.dart
@@ -9,45 +9,181 @@ class Detail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 250.0,
-                floating: false,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: 100,
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                SliverOverlapAbsorber(
+                    child: SliverSafeArea(
+                      top: false,
+                      sliver: SliverAppBar(
+                        expandedHeight: 250,
+                        pinned: true,
+                        flexibleSpace: FlexibleSpaceBar(
+                          title: Text(
+                            cardInfo.name,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          background: Image.network(
+                            cardInfo.cardImages[0].imageUrl,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
                     ),
-                    child: Text(
-                      "HHHHH",
-                      textScaleFactor: .8,
-                    ),
-                  ),
-                  background: Image.network(
-                    cardInfo.cardImages[0].imageUrl,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-            ];
-          },
-          body: Container(
-            margin: EdgeInsets.only(top: 16),
-            child: Center(
-              child: Card(
-                child: Column(
-                  children: <Widget>[
-                    Text(cardInfo.desc),
-                    Text(cardInfo.type),
-                    Text(cardInfo.archetype)
-                  ],
-                ),
-              ),
-            ),
-          )),
+                    handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                        context))
+              ],
+          body: SingleChildScrollView(
+              child: Container(
+                  margin:
+                      EdgeInsets.only(top: 16, bottom: 16, right: 8, left: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 16, horizontal: 8),
+                              child: Text(
+                                "Description",
+                                style: TextStyle(
+                                    fontSize: 22, fontFamily: 'RobotoBold'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                cardInfo.desc,
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 16, horizontal: 8),
+                              child: Text(
+                                "Sets",
+                                style: TextStyle(
+                                    fontSize: 22, fontFamily: 'RobotoBold'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'Name: ${cardInfo.cardSets[0].setName}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'Code: ${cardInfo.cardSets[0].setCode}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'Rarity: ${cardInfo.cardSets[0].setRarity}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'Price: ${cardInfo.cardSets[0].setPrice}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 16, horizontal: 8),
+                              child: Text(
+                                "Prices",
+                                style: TextStyle(
+                                    fontSize: 22, fontFamily: 'RobotoBold'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'Card Market: ${cardInfo.cardPrices.cardMarketPrice}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'TCG Player: ${cardInfo.cardPrices.tcgPlayerPrice}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'Ebay: ${cardInfo.cardPrices.ebayPrice}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, bottom: 16),
+                              child: Text(
+                                'Amazon: ${cardInfo.cardPrices.amazonPrice}',
+                                style: TextStyle(
+                                    fontSize: 18, fontFamily: 'RobotoRegular'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )))),
     );
   }
 }
